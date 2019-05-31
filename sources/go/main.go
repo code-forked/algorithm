eac86a5faed1dbca6c60bcad04b48bd4a4fa2e88
ruyue <99316632@qq.com>
2019-05-31 19:11:53 +0800

    线性表顺序存储

--- sources/go/main.go
+++ sources/go/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"algorithm/array"
 	"algorithm/list"
+	"fmt"
 )
 
 
 func main() {
+
+	al := list.NewArrayList()
+	al.Show()
+
+	al.Append(10)
+	al.Append(12)
+	al.Show()
+
+	err := al.Insert(1,15)
+	fmt.Println(err)
+	al.Show()
+
+	err = al.Insert(2,13)
+	fmt.Println(err)
+	al.Show()
+	fmt.Println(al.Length())
+
+
+
 }
