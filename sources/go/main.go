ee72e8ab51596a3708d5306bb5606a45de02fa9e
ruyue <99316632@qq.com>
2019-06-01 00:25:13 +0800

    顺序表BUG修正

--- sources/go/main.go
+++ sources/go/main.go
@@ -5,24 +5,37 @@ import (
 	"fmt"
 )
 
+func testSequenList() {
+
+	sl := list.NewSequenList()
+	sl.Show()
+
+	sl.Append(7)
+	sl.Append(9)
+	sl.Show()
+
+	sl.Insert(1,6)
+	sl.Show()
+
+	sl.Pop()
+	sl.Show()
+
+	// err := al.Insert(1,15)
+	// fmt.Println(err)
+	// al.Show()
+
+	// err = al.Insert(2,13)
+	// fmt.Println(err)
+	// al.Show()
+	// fmt.Println(al.Length())
+
+}
 
 func main() {
 
-	al := list.NewArrayList()
-	al.Show()
+	fmt.Println("start run...")
 
-	al.Append(10)
-	al.Append(12)
-	al.Show()
-
-	err := al.Insert(1,15)
-	fmt.Println(err)
-	al.Show()
-
-	err = al.Insert(2,13)
-	fmt.Println(err)
-	al.Show()
-	fmt.Println(al.Length())
+	testSequenList()				// 测试顺序表	
 
 
 
